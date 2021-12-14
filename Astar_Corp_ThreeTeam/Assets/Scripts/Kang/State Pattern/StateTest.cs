6e8865f3b731a6668e4ec095627f2b90e3e4a6b0
zzaho120 <zzaho120@gmail.com>
2021-12-14 13:58:29 +0900

    211214_강주수_상태패턴

---
+++ Astar_Corp_ThreeTeam/Assets/Scripts/Kang/State Pattern/StateTest.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveState : StateBase
+{
+    private float speed = 3f;
+    private Rigidbody rigid;
+    private float startTime;
+    public MoveState(FSM fsm)
+    {
+        this.fsm = fsm;
+
+        if (this.fsm != null)
+            rigid = fsm.GetComponent<Rigidbody>();
+
+    }
+    public override void Enter()
+    {
+        startTime = Time.time;
+    }
+
+    public override void Exit()
+    {
+        if (rigid != null)
+            rigid.velocity = Vector3.zero;
+    }
+
+    public override void Update()
+    {
+        if (rigid != null)
+            rigid.velocity = new Vector3(speed * 3f * Time.fixedDeltaTime, 0f, 0f);
+
+        if (Time.time > startTime + 3f)
+            fsm.ChangeState(STATE.Idle);
+    }
+}
