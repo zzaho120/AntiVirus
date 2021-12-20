b59d740a0793d03f49ec2a67cdeaf3d4ce14e886
zzaho120 <zzaho120@gmail.com>
2021-12-20 16:10:18 +0900

    211220_강주수_이동범위그리기
    이동범위 그리기 구현
    
    클릭시 이동하게 수정

--- Astar_Corp_ThreeTeam/Assets/Scripts/Kang/Tile/PlayerTest.cs
+++ Astar_Corp_ThreeTeam/Assets/Scripts/Kang/Tile/PlayerTest.cs
@@ -7,6 +7,10 @@ public class PlayerTest : MonoBehaviour
     public Vector3 tileIdx;
     public TileBase currentTile;
 
+    public int moveDistance;
+    public bool isMove;
+    private Dictionary<Vector3, TileBase> moveDics =
+        new Dictionary<Vector3, TileBase>();
     public void Init()
     {
         var dics = BattleMgr.Instance.tileMgr.tileDics;
@@ -19,7 +23,7 @@ public class PlayerTest : MonoBehaviour
             {
                 currentTile = tile;
                 break;
-            }    
+            }
         }
     }
 
@@ -51,11 +55,24 @@ public class PlayerTest : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Alpha0))
         {
-            BattleMgr.Instance.aStar.InitAStar(currentTile.tileIdx, new Vector3(4, 6, 1));
+            BattleMgr.Instance.aStar.InitAStar(currentTile.tileIdx, new Vector3(5, 6, 6));
             StartCoroutine(CoMove());
         }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    MoveMode();
+                }
+            }
+        }
     }
-    
+
     private void MoveTile(Vector3 nextIdx)
     {
         foreach (var tile in currentTile.adjNodes)
@@ -72,7 +89,6 @@ public class PlayerTest : MonoBehaviour
     private IEnumerator CoMove()
     {
         var path = BattleMgr.Instance.aStar.pathList;
-        var idx = 0;
         while (path.Count > 0)
         {
             var aStarTile = path.Pop();
@@ -80,5 +96,67 @@ public class PlayerTest : MonoBehaviour
             MoveTile(aStarTile.tileBase.tileIdx);
             yield return new WaitForSeconds(1f);
         }
+    }
+
+    public void MoveMode()
+    {
+        isMove = !isMove;
+        var ren = GetComponent<MeshRenderer>();
+
+        if (isMove)
+        {
+            ren.material.color = Color.red;
+            MoveFloodFill();
+        }
+        else
+        {
+            ren.material.color = Color.white;
+            foreach (var tile in moveDics)
+            {
+                var tileRen = tile.Value.GetComponent<MeshRenderer>();
+                if (tileRen == null)
+                {
+                    var obj = tile.Value.transform.GetChild(0);
+                    tileRen = obj.GetComponent<MeshRenderer>();
+                }
+                tileRen.material.color = Color.white;
+            }
+        }
+    }
+
+    private void MoveFloodFill()
+    {
+        moveDics.Clear();
+        moveDics.Add(tileIdx, currentTile);
+
+        var cnt = 0;
+        foreach (var adjNode in currentTile.adjNodes)
+        {
+            CheckMoveRange(adjNode, cnt);
+        }
+    }
+
+    private void CheckMoveRange(TileBase tile, int cnt)
+    {
+        if (cnt >= moveDistance)
+            return;
+
+        if (!moveDics.ContainsKey(tile.tileIdx))
+            moveDics.Add(tile.tileIdx, tile);
+        else
+            return;
+
+        var ren = tile.GetComponent<MeshRenderer>();
+        if (ren == null)
+        {
+            var obj = tile.transform.GetChild(0);
+            ren = obj.GetComponent<MeshRenderer>();
+        }
+        ren.material.color = Color.blue;
+
+        cnt++;
+        Debug.Log($"{tile.tileIdx}, {cnt}");
+        foreach (var adjNode in tile.adjNodes)
+            CheckMoveRange(adjNode, cnt);
     }
 }
