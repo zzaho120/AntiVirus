fbbc7541f7438794e5820a429e422e5612c8b6c0
estherSujin <shineetm0718@naver.com>
2022-01-11 09:02:11 +0900

    220111_홍수진_트럭UI

--- Astar_Corp_ThreeTeam/Assets/Scripts/Choi/NonBattleMap/PlayerDataMgr.cs
+++ Astar_Corp_ThreeTeam/Assets/Scripts/Choi/NonBattleMap/PlayerDataMgr.cs
@@ -33,9 +33,7 @@ public class PlayerDataMgr : Singleton<PlayerDataMgr>
     public Dictionary<string, int> truckConsumablesNum = new Dictionary<string, int>();
 
     //캐릭터 데이터.
-    public Dictionary<int, CharacterStats> currentSquad = new Dictionary<int, CharacterStats>();    //현재 소유한 캐릭터들.
-    public Dictionary<int, CharacterStats> boardingSquad = new Dictionary<int, CharacterStats>();   //트럭에 탑승한 캐릭터들.
-    public Dictionary<int, CharacterStats> battleSquad = new Dictionary<int, CharacterStats>();     //전투에 나갈 캐릭터들.
+
     private void Start()
     {
         //PlayerPrefs.DeleteAll();
