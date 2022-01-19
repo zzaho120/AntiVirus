2548f4ba0294ce84554c2fd18b83c14fcbe8e070
estherSujin <shineetm0718@naver.com>
2022-01-19 10:59:25 +0900

    220119_홍수진_SO추가
    벙커 에쓰오

---
+++ Astar_Corp_ThreeTeam/Assets/Editor/CreateSO/BunkerSO.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class BunkerSO
+{
+    private static string bunkerCSVPath = "/Resources/Choi/BunkerDataTable.csv";
+
+    [MenuItem("Utilities/Generate Bunkers")]
+    public static void GenerateCharacters()
+    {
+        string[] files = Directory.GetFiles("Assets/Resources/Choi/Datas/Bunkers");
+        foreach (string file in files)
+        {
+            File.Delete(file);
+            //Debug.Log($"{file} is deleted.");
+        }
+
+        int bunkerNum = 0;
+        using (FileStream fs = new FileStream(Application.dataPath + bunkerCSVPath, FileMode.Open, FileAccess.Read))
+        {
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                bool isFirst = true;
+
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (isFirst)
+                    {
+                        isFirst = false;
+                        continue;
+                    }
+                    string[] splitData = line.Split(',');
+
+                    Bunker bunker = ScriptableObject.CreateInstance<Bunker>();
+                    bunker.id       = splitData[0];
+                    bunker.type     = int.Parse(splitData[1]);
+                    bunker.name     = splitData[2];
+
+                    bunker.level1   = int.Parse(splitData[3]);
+                    bunker.level2   = int.Parse(splitData[4]);
+                    bunker.level3   = int.Parse(splitData[5]);
+                    bunker.level4   = int.Parse(splitData[6]);
+                    bunker.level5   = int.Parse(splitData[7]);
+
+                    AssetDatabase.CreateAsset(bunker, $"Assets//Resources/Choi/Datas/Bunkers/{bunker.name}.asset");
+                    bunkerNum++;
+                }
+                sr.Close();
+            }
+            fs.Close();
+            AssetDatabase.SaveAssets();
+            Debug.Log($"벙커SO가 {bunkerNum}개 생성되었습니다.");
+        }
+    }
+}
---
+++ Astar_Corp_ThreeTeam/Assets/Scripts/Hong/DataTable/Datas/Bunker.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bunker : ScriptableObject
+{
+    public string id;
+    public int type;
+    public new string name;
+
+    public int level1;
+    public int level2;
+    public int level3;
+    public int level4;
+    public int level5;
+}
